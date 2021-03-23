7d04e89a45a04387ce916d5887a5a6d180b2b956
Daniel Rezende <daniel.zende@gmail.com>
2021-03-23 11:07:46 -0300

    Configurações do miragejs, para seeds e para as rotas get e post

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models:{
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de Website',
+          type: 'income',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'outcome',
+          category: 'casa',
+          amount: 1200,
+          createdAt: new Date('2021-02-14 09:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transactions 1',
-          amount: 400,
-          type: 'income',
-          category: 'Developer',
-          createdAt: new Date()
-        },
-        {
-          id: 1,
-          title: 'Transactions 1',
-          amount: -200,
-          type: 'outcome',
-          category: 'Food',
-          createdAt: new Date()
-        },
-      ]
+      return this.schema.all('transaction')
     })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    })
+
   },
 })
 
